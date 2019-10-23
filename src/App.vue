051a4fb75b223ae70e7239d89891eedf2b282549
lzce <487997188@qq.com>
2019-10-23 16:13:03 +0800

    init my project

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="app-container">
+    <!-- 顶部header区域 -->
+    <mt-header class="app-" fixed title="前端黑马52 vue项目"></mt-header>
+    <h1>哈哈哈</h1>
+    <!-- 中间路由部分 -->
+
+    <!-- 底部tab部分 -->
+    <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">消息</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">通讯录</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">设置</span>
+			</a>
+		</nav>
+
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.app-container {
+  padding-top: 40px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,27 @@
+//入口文件
+import Vue from 'vue'
+// import './lib/mui/js/mui.js'
+
+//按需导入 mint-ui 组件
+import {Header, Tabbar, TabItem } from 'mint-ui'
+
+//手动安装组件
+Vue.component(Header.name, Header) 
+Vue.component(Tabbar.name, Tabbar)
+Vue.component(TabItem.name, TabItem)
+
+//导入app组件
+import app from './App.vue'
+
+//导入样式
+import 'mint-ui/lib/style.css'
+import './lib/mui/css/mui.min.css'
+
+
+const vm = new Vue({
+  el: '#app',
+  render: c => c(app),
+})
+
+
+
